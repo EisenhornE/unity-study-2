b0345311eab8ae57a5c75d1d4521f68440462728
Carl John Ignacio V. Nicor <cjinicor@gmail.com>
2024-01-12 23:35:11 +0800

    init commit
    initial commit for Data Persistence and OOP in Unity

---
+++ unity-project-2/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void SetPlayerName()
+    {
+        inputField.text = PlayerPrefs.GetString("name");
+    }
+}
